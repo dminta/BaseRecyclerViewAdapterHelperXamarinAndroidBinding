6ea637f5eec20f4c2913fd5b921bb60abee9ec10
dminta <dominik@minta.it>
2018-05-12 00:03:12 +0200

    EmptyView sample

---
+++ BaseRecyclerViewAdapterHelper.Sample/Adapter/QuickAdapter.cs
@@ -0,0 +1,31 @@
+﻿using BaseRecyclerViewAdapterHelper.Sample.Data;
+using BaseRecyclerViewAdapterHelper.Sample.Entity;
+using Chad.BaseRecyclerViewAdapterHelper.Adapter.Base;
+
+namespace BaseRecyclerViewAdapterHelper.Sample.Adapter
+{
+    public class QuickAdapter : BaseQuickAdapter
+    {
+        public QuickAdapter(int dataSize) : base(Resource.Layout.layout_animation, DataServer.GetSampleData(dataSize)) { }
+
+        protected override void Convert(BaseViewHolder helper, Java.Lang.Object item)
+        {
+            var status = item as Status;
+            
+            switch (helper.LayoutPosition % 3)
+            {
+                case 0:
+                    helper.SetImageResource(Resource.Id.img, Resource.Mipmap.animation_img1);
+                    break;
+                case 1:
+                    helper.SetImageResource(Resource.Id.img, Resource.Mipmap.animation_img2);
+                    break;
+                case 2:
+                    helper.SetImageResource(Resource.Id.img, Resource.Mipmap.animation_img3);
+                    break;
+            }
+            helper.SetText(Resource.Id.tweetName, "Hoteis in Rio de Janeiro");
+            helper.SetText(Resource.Id.tweetText, "O ever youthful,O ever weeping");
+        }
+    }
+}
---
+++ BaseRecyclerViewAdapterHelper.Sample/EmptyViewUseActivity.cs
@@ -0,0 +1,84 @@
+﻿
+using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using BaseRecyclerViewAdapterHelper.Sample.Adapter;
+using BaseRecyclerViewAdapterHelper.Sample.Base;
+using BaseRecyclerViewAdapterHelper.Sample.Data;
+
+namespace BaseRecyclerViewAdapterHelper.Sample
+{
+    [Activity]
+    public class EmptyViewUseActivity : BaseActivity, View.IOnClickListener
+    {
+        RecyclerView _recyclerView;
+        QuickAdapter _quickAdapter;
+        View _notDataView;
+        View _errorView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetBackBtn();
+            SetTitle("EmptyView Use");
+            SetContentView(Resource.Layout.activity_empty_view_use);
+            FindViewById(Resource.Id.btn_reset).SetOnClickListener(this);
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.rv_list);
+            _recyclerView.HasFixedSize = true;
+            _recyclerView.SetLayoutManager(new LinearLayoutManager(this));
+
+            _notDataView = LayoutInflater.Inflate(Resource.Layout.empty_view, (ViewGroup)_recyclerView.Parent, false);
+            _notDataView.Click += (s, e) => OnRefresh();
+
+            _errorView = LayoutInflater.Inflate(Resource.Layout.error_view, (ViewGroup)_recyclerView.Parent, false);
+            _errorView.Click += (s, e) => OnRefresh();
+
+            InitAdapter();
+            OnRefresh();
+        }
+
+        void InitAdapter()
+        {
+            _quickAdapter = new QuickAdapter(0);
+            _recyclerView.SetAdapter(_quickAdapter);
+        }
+
+        public void OnClick(View v)
+        {
+            _error = true;
+            _noData = true;
+            _quickAdapter.SetNewData(null);
+            OnRefresh();
+        }
+
+        bool _error = true;
+        bool _noData = true;
+
+        void OnRefresh()
+        {
+            _quickAdapter.SetEmptyView(Resource.Layout.loading_view, (ViewGroup)_recyclerView.Parent);
+            new Handler().PostDelayed(() =>
+            {
+                if (_error)
+                {
+                    _quickAdapter.EmptyView = _errorView;
+                    _error = false;
+                }
+                else
+                {
+                    if (_noData)
+                    {
+                        _quickAdapter.EmptyView = _notDataView;
+                        _noData = false;
+                    }
+                    else;
+                    {
+                        _quickAdapter.SetNewData(DataServer.GetSampleData(10));
+                    }
+                }
+
+            }, 1000);
+        }
+    }
+}
